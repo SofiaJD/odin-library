2340375a411a6b9a58602d5b9bf6d72c8d796686
Sofía J. D. <59852487+SofiaJD@users.noreply.github.com>
2024-03-27 14:53:47 -0400

    Add function that show the books

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const txtTitle = document.querySelector('#title');
 const txtAuthor = document.querySelector('#author');
 const btnAdd = document.querySelector('#btnAdd');
+const container = document.querySelector('#container');
 
 const myLibrary = [];
 
-function Book(title, author) {
+myLibrary.push(new Book('Libro 1', 'author'));
+myLibrary.push(new Book('Libro 2', 'author'));
+myLibrary.push(new Book('Libro 3', 'author'));
+
+function Book(title, author) 
+{
     this.title = title;
     this.author = author;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary() 
+{
     title = txtTitle.value;
     author = txtAuthor.value;
 
@@ -18,8 +25,22 @@ function addBookToLibrary() {
     console.log(myLibrary);
 }
 
-btnAdd.addEventListener('click', function(e) {
+btnAdd.addEventListener('click', function(e) 
+{
     addBookToLibrary();
     e.preventDefault();
-
 });
+
+function showBooks()
+{
+    myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.setAttribute('id', 'card');
+
+        card.innerHTML = `<h2>${book.title}</h2> <p>${book.author}</p>`
+
+        container.appendChild(card);
+    });
+}
+
+showBooks();
