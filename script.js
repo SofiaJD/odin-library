6a9283289946a73fca56ca0937cd8b7434a3eccc
Sofia J. D. <sofiaduran1448@gmail.com>
2024-04-30 23:46:12 -0400

    Add delete books

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const btnAdd = document.querySelector('#btnAdd');
 const container = document.querySelector('#cardContainer');
 const buttonModal = document.querySelector('#buttonModal');
+const buttonCloseModal = document.querySelector('#btnClose');
 const modal = document.querySelector('dialog');
 const txtTitle = document.querySelector('#title');
 const txtAuthor = document.querySelector('#author');
 const txtPages = document.querySelector('#pages');
+const cards = document.querySelectorAll('.card');
 
 const myLibrary = [];
 
-myLibrary.push(new Book('Libro 1', 'author', '200', 'Read'));
-myLibrary.push(new Book('Libro 2', 'author', '400', 'Not Read'));
-myLibrary.push(new Book('Libro 3', 'author', '300', 'Read'));
+myLibrary.push(new Book('Book 1', 'author', '200', 'Read'));
+myLibrary.push(new Book('Book 2', 'author', '400', 'Not Read'));
+myLibrary.push(new Book('Book 3', 'author', '300', 'Read'));
 
 function Book(title, author, pages, status) 
 {
@@ -22,6 +24,11 @@ function Book(title, author, pages, status)
 
 buttonModal.addEventListener('click', function() {
     modal.showModal();
+});
+
+buttonCloseModal.addEventListener('click', function()
+{
+    modal.close();
 });
 
 function addBookToLibrary() 
@@ -45,9 +52,8 @@ btnAdd.addEventListener('click', function(e)
     modal.close();
 });
 
-function showBooks()
-{
-    myLibrary.forEach(book => {
+function showBooks() {
+    myLibrary.forEach((book, index) => {
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
 
@@ -56,19 +62,47 @@ function showBooks()
         const details = document.createElement('div');
         details.setAttribute('class', 'details');
 
-        if(book.status == 'Read')
-        {
+        if (book.status == 'Read') {
             details.innerHTML = `<p>${book.pages} pages</p> <p class="read">${book.status}</p>`;
-        }
-        else
-        {
+        } else {
             details.innerHTML = `<p>${book.pages} pages</p> <p class="notRead">${book.status}</p>`;
         }
 
         card.appendChild(details);
 
+        const indexPara = document.createElement('p');
+        indexPara.setAttribute('class', 'index');
+        indexPara.innerText = index;
+
+        card.appendChild(indexPara);
+
+        const btnDelete = document.createElement('button');
+        btnDelete.setAttribute('class', 'btnDelete');
+
+        const deleteIcon = document.createElement('span');
+        deleteIcon.setAttribute('class', 'material-symbols-outlined');
+        deleteIcon.innerText = 'heart_minus';
+
+        btnDelete.appendChild(deleteIcon);
+
+        card.appendChild(btnDelete);
+
         container.appendChild(card);
+
+        btnDelete.addEventListener('click', function() {
+            myLibrary.splice(index, 1);
+            container.innerHTML = '';
+            showBooks();
+        });
     });
 }
 
-showBooks();
+
+if(myLibrary.length < 1)
+{
+    container.innerHTML = 'There are no registered books.'
+}
+else 
+{
+    showBooks();
+}
